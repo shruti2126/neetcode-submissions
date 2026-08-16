2bf55a82b1165229a431d64d6e1b072f0287f14d
Shruti <60984268+shruti2126@users.noreply.github.com>
2026-08-16 18:10:11 +0530

    Add: python-sort-custom - submission-0

---
+++ Python Coding Interviews/python-sort-custom/submission-0.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def sort_words(words: List[str]) -> List[str]:
+    return sorted(words, key=lambda word : len(word), reverse=True)
+
+def sort_numbers(numbers: List[int]) -> List[int]:
+    new_list = sorted(numbers, key=lambda num : abs(num), reverse=False)
+    return new_list
+
+
+# do not modify below this line
+print(sort_words(["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]))
+
+print(sort_numbers([1, -5, -3, 2, 4, 11, -19, 9, -2, 5, -6, 7, -4, 2, 6]))
