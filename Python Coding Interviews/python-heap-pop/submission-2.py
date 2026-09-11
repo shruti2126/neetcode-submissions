60b42922726253f4b6eabf7bf689ebcce1ea160c
Shruti <60984268+shruti2126@users.noreply.github.com>
2026-09-11 14:42:20 +0530

    Add: python-heap-pop - submission-2

---
+++ Python Coding Interviews/python-heap-pop/submission-2.py
@@ -0,0 +1,18 @@
+import heapq
+from typing import List
+
+
+def heap_pop(heap: List[int]) -> List[int]:
+    min_heap_lst = []
+    
+    while len(heap) > 0:
+        popped = heapq.heappop(heap)
+        min_heap_lst.append(popped)
+    return min_heap_lst
+
+
+
+# do not modify below this line
+print(heap_pop([1, 2, 3]))
+print(heap_pop([1, 3, 2]))
+print(heap_pop([6, 7, 8, 12, 9, 10]))
